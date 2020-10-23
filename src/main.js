d601d0a6e8d518a5113460af64c5c9962f5ee4a1
xing <xing.yancy@foxmail.com>
2020-10-23 17:35:16 +0800

    学生管理系统

--- src/main.js
+++ src/main.js
@@ -1,10 +1,18 @@
-import Vue from 'vue'
-import App from './App.vue'
-import router from './router'
+import Vue from 'vue';
+import App from './App.vue';
+import router from './router';
+import store from './store';
+import api from './api';
+import cookie from './util/cookie';
+import toast from './util/toast/index.js';
 
-Vue.config.productionTip = false
+Vue.config.productionTip = false;
+Vue.prototype.$api=api;
+Vue.prototype.cookie=cookie;
+Vue.prototype.$toast=toast;
 
 new Vue({
   router,
+  store,
   render: h => h(App)
 }).$mount('#app')
--- src/router/index.js
+++ src/router/index.js
@@ -1,29 +1,70 @@
 import Vue from 'vue'
 import VueRouter from 'vue-router'
-import Home from '../views/Home.vue'
+
+import main from '@/views/main/index'
+import login from '@/views/login/index'
+import logon from '@/views/logon/index'
+import error from '@/views/error/index'
+import stuAdd from '@/views/main/stuAdd/index'
+import stuList from '@/views/main/stuList/index'
+
+import cookie from '@/util/cookie'
 
 Vue.use(VueRouter)
 
 const routes = [
   {
-    path: '/',
-    name: 'Home',
-    component: Home
+    path:"/main",
+    component:main,
+    redirect:'/main/stulist',
+    children:[
+      {
+        path:'stuadd',
+        component:stuAdd,
+      },
+      {
+        path:'stuList',
+        component:stuList,
+      }
+    ]
   },
   {
-    path: '/about',
-    name: 'About',
-    // route level code-splitting
-    // this generates a separate chunk (about.[hash].js) for this route
-    // which is lazy-loaded when the route is visited.
-    component: () => import(/* webpackChunkName: "about" */ '../views/About.vue')
-  }
+    path:"/login",
+    component:login,
+  },
+  {
+    path:"/logon",
+    component:logon,
+  },
+  {
+    path:"/error",
+    component:error,
+  },
 ]
 
 const router = new VueRouter({
   mode: 'history',
   base: process.env.BASE_URL,
-  routes
+  routes,
+  linkExactActiveClass:'active'
+})
+
+router.beforeEach((to,from,next)=>{
+  if(to.path === '/'){
+    next('/main');
+  }else if(to.matched.length === 0){
+    next('/error');
+  }
+  
+  if(to.matched[0] && to.matched[0].path === '/main'){
+    const username = cookie.getCookie('username');
+    if(username){
+      next();
+    }else{
+      next('/login');
+    }
+  }
+  next();
 })
 
 export default router
